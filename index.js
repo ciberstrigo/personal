950ca5c1e7c961c14087485453072113be452b60
Grand Master Strigo <a7ef9@176359.simplecloud.ru>
2018-04-23 00:48:12 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.listen(8080, '85.143.173.216');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
